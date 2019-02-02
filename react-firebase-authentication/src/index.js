879313c2390192612d030f7a734664ecd3967c8d
ksterle <krunoslavsterle@gmail.com>
2019-02-02 20:23:37 +0100

    - adding firebase

--- react-firebase-authentication/src/index.js
+++ react-firebase-authentication/src/index.js
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App';
+import Firebase, { FirebaseContext } from './components/Firebase';
+
+ReactDOM.render(
+    // Doing it this way, we can be assured that Firebase is only instantiated once and that it is injected via
+    // React’s Context API to React’s component tree
+    <FirebaseContext.Provider value={new Firebase()}>
+        <App />
+    </FirebaseContext.Provider>
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
